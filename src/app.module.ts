696730a6ef8126e1b4449a43afee522045413fa5
worstp <mikaelmuniz2001@gmail.com>
2022-10-08 18:31:31 -0300

    feat: adding a module to fetch env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PersonModule } from './counter/counter.module';
 
 @Module({
-    imports: [MongooseModule.forRoot('mongodb+srv://mackleaps:lfs123@cluster0.1sv57r9.mongodb.net/?retryWrites=true&w=majority'), PersonModule],
+    imports: [ConfigModule.forRoot({
+        envFilePath: '.env'
+    }), MongooseModule.forRoot(process.env.URL), PersonModule],
     controllers: [AppController],
     providers: [AppService],
 })
